e2b7fb2a662468444982cddadc87b7d6d0c2cbeb
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2019-09-26 17:19:45 +0200

    Stop on errors.

--- test/test.lua
+++ test/test.lua
@@ -36,36 +36,37 @@ end
 
 -- Open tempsensor
 iResult = tempsensor.rawhid_open(1, 0x16C0, 0x0480, 0xFFAB, 0x0200)
-
-if iResult <= 0 then
-    print(string.format('%d tempsensor found', iResult))
+if iResult<0 then
+  error(string.format('Failed to detect a tempsensor: %d', iResult))
+elseif iResult==0 then
+  print('No tempsensor found.')
 else
-    print(string.format('%d tempsensor found', iResult))
-end
+  print(string.format('%d tempsensor found', iResult))
 
--- Read temperature
-while i do
+  -- Read temperature
+  while i do
     iResult, buf = tempsensor.rawhid_recv(0, 64, 220);
 
     if iResult < 0 then
-	print('Error reading tempsensor')
-	tempsensor.rawhid_close(0)
+      tempsensor.rawhid_close(0)
+      error('Error reading tempsensor')
     end
 
     if iResult == 64 then
-	-- Dump 64byte
-	--hexdump(buf)
+      -- Dump 64byte
+      --hexdump(buf)
 
-	-- Get temp bytes 5 and 6 of 64byte string
-	byte0, byte1 = string.byte(buf, 5, 6)
-	temp = (byte0 + (byte1*255))/10
+      -- Get temp bytes 5 and 6 of 64byte string
+      byte0, byte1 = string.byte(buf, 5, 6)
+      temp = (byte0 + (byte1*255))/10
 
-	-- Get Number of sensors connected to PC
-	sensorCnt = string.byte(buf, 2)
+      -- Get Number of sensors connected to PC
+      sensorCnt = string.byte(buf, 2)
 
-	-- Get Number of sensor measuring temperature
-	sensorNr = string.byte(buf, 1)
+      -- Get Number of sensor measuring temperature
+      sensorNr = string.byte(buf, 1)
 
-	print(string.format('Sensor nr %d of %d temp: %0.1f*C',sensorNr, sensorCnt, temp))
+      print(string.format('Sensor nr %d of %d temp: %0.1f*C',sensorNr, sensorCnt, temp))
     end
+  end
 end
